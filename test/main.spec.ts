39d08ade508f20da0aa5bda6f01d3c31a92929af
Ruslan Gainutdinov <ruslan.gainutdinov@maas.global>
2020-03-17 00:25:03 +0200

    Using emitters

--- test/main.spec.ts
+++ test/main.spec.ts
@@ -11,22 +11,20 @@ import {
   StepBody,
 } from 'workflow-es'
 
-const timestamp = new Date().getTime()
-const mem = new Int32Array(new SharedArrayBuffer(4));
-Atomics.store(mem, 0, 0);
+const emitter = new EventEmitter();
 
-class PingBack extends StepBody {
+class EmitPing extends StepBody {
   public run(context: StepExecutionContext): Promise<ExecutionResult> {
     console.info('Pinging', context.workflow.id)
-    Atomics.add(mem, 0, 255)
-    Atomics.notify(mem, 0, 999)
+    emitter.emit('ping')
     return exec.next()
   }
 }
 
-class Eol extends StepBody {
+class EmitDone extends StepBody {
   public run(context: StepExecutionContext): Promise<ExecutionResult> {
-    console.info('Done', context.workflow.id)
+    console.info('Sending done', context.workflow.id)
+    emitter.emit('done')
     return exec.next()
   }
 }
@@ -42,54 +40,46 @@ class LogMessage extends StepBody {
 
 class SampleWorkflow implements WorkflowBase<any> {
   public id: string = 'test1'
-  public version: number = timestamp
+  public version: number = 1
 
   public build(builder: WorkflowBuilder<any>) {
     builder
       .startWith(LogMessage)
       .input((step, _) => (step.message = 'Waiting for event...'))
-      .then(PingBack)
+      .then(EmitPing)
       .waitFor('myEvent', _ => '0')
       .output((step, data) => (data.externalValue = step.eventData))
       .then(LogMessage)
       .input((step, data) => (step.message = 'The event data is ' + data.externalValue))
-      .then(Eol)
+      .then(LogMessage)
+      .input((step, _) => (step.message = 'Complete'))
+      .then(EmitDone)
   }
 }
 
-function* gen() {
-  while (Atomics.load(mem, 0) === 0) {
-    yield false
-  }
-  return true
-}
 
 describe('main.ts', () => {
   it('can start workflow', async () => {
    
     const config = configureWorkflow()
-    assert.notEqual(config, null)
+    assert.ok(config)
     const host = config.getHost()
-    assert.notEqual(host, null)
+    assert.ok(host)
     host.registerWorkflow(SampleWorkflow)
     await host.start()
-    const id = await host.startWorkflow('test1', timestamp, null)
+
+    emitter.on('ping', () => {
+      console.info("Sending event")
+      host.publishEvent('myEvent', '0', "Hi!", new Date())
+    })
+
+    emitter.on('done', () => {
+      console.info("Workflow done")
+      assert.ok(true)
+    })
+
+    const id = await host.startWorkflow('test1', 1, null)
     assert.ok(id)
     console.log('Started workflow: ' + id)
-    const iter = gen()
-    let finished = await iter.next()
-    console.info(iter.next())
-    //await new Promise(resolve => setTimeout(() => resolve(), 5000));
-    return new Promise(async resolve => {
-      let tries = 0
-      const startTime = new Date().getTime()
-      while (!finished.done && tries <1000) { tries++; finished = await iter.next(); console.info(tries + ", ", finished) }
-      console.info("Received ping " + id + " in " + (new Date().getTime() - startTime) + "ms")
-      console.info('Sending event ' + id, Atomics.load(mem, 0))
-      host.publishEvent('myEvent', '0', "Hi!", new Date())
-      console.info('Waiting for event processing ' + id, Atomics.load(mem, 0))
-      await new Promise(resolve => setTimeout(() => resolve(), 5000));
-      resolve(1)
-    })
-  }).timeout(20000)
+  }).timeout(10000)
 })
